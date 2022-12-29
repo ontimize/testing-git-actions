2ba3e47f5e3b48a4b857533b2428b66f80e58152
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-12-29 16:27:09 +0100

    Create and modify GitHub Actions

--- src/main/java/com/ontimize/Main.java
+++ src/main/java/com/ontimize/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Add messages to perform a code commit
+        // Add messages to perform a code commit
         System.out.println("Hello world!");
     }
 }
